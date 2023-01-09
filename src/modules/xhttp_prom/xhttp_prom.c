22dc0dafc38c48d84a6986cd7f5ef7038dc10989
root <root@ip-172-31-58-88.us-west-1.compute.internal>
2023-01-09 22:17:26 +0000

    xhttp_prom: adding support for variables in
    counter inc

--- src/modules/xhttp_prom/xhttp_prom.c
+++ src/modules/xhttp_prom/xhttp_prom.c
@@ -87,6 +87,7 @@ static int w_prom_histogram_observe_l2(struct sip_msg* msg, char *pname, char* p
 static int w_prom_histogram_observe_l3(struct sip_msg* msg, char *pname, char* pnumber, char *l1, char *l2, char *l3);
 static int fixup_metric_reset(void** param, int param_no);
 static int fixup_counter_inc(void** param, int param_no);
+static int fixup_free_counter_inc(void** param, int param_no);
 
 int prom_counter_param(modparam_t type, void *val);
 int prom_gauge_param(modparam_t type, void *val);
@@ -144,13 +145,13 @@ static cmd_export_t cmds[] = {
 	{"prom_gauge_reset", (cmd_function)w_prom_gauge_reset_l3, 4, fixup_metric_reset,
 	 0, ANY_ROUTE},
 	{"prom_counter_inc", (cmd_function)w_prom_counter_inc_l0, 2, fixup_counter_inc,
-	 0, ANY_ROUTE},
+	 fixup_free_counter_inc, ANY_ROUTE},
 	{"prom_counter_inc", (cmd_function)w_prom_counter_inc_l1, 3, fixup_counter_inc,
-	 0, ANY_ROUTE},
+	 fixup_free_counter_inc, ANY_ROUTE},
 	{"prom_counter_inc", (cmd_function)w_prom_counter_inc_l2, 4, fixup_counter_inc,
-	 0, ANY_ROUTE},
+	 fixup_free_counter_inc, ANY_ROUTE},
 	{"prom_counter_inc", (cmd_function)w_prom_counter_inc_l3, 5, fixup_counter_inc,
-	 0, ANY_ROUTE},
+	 fixup_free_counter_inc, ANY_ROUTE},
 	{"prom_gauge_set", (cmd_function)w_prom_gauge_set_l0, 2, fixup_metric_reset,
 	 0, ANY_ROUTE},
 	{"prom_gauge_set", (cmd_function)w_prom_gauge_set_l1, 3, fixup_metric_reset,
@@ -949,14 +950,14 @@ static int fixup_counter_inc(void** param, int param_no)
 	}
 }
 
-/* static int fixup_free_counter_inc(void** param, int param_no) */
-/* { */
-/* 	if (param_no == 1 || param_no == 2) { */
-/* 		return fixup_free_spve_igp(param, param_no); */
-/* 	} else { */
-/* 		return fixup_free_spve_null(param, 1); */
-/* 	} */
-/* } */
+static int fixup_free_counter_inc(void** param, int param_no)
+{
+	if (param_no == 1 || param_no == 2) {
+		return fixup_free_spve_igp(param, param_no);
+	} else {
+		return fixup_free_spve_null(param, 1);
+	}
+}
 
 /**
  * @brief Add an integer to a counter (No labels).
